edf851ea4a506cb8025f88bcda2c991c886e0dfc
nguyenthilieu15051993-cell <nguyenthilieu15051993@gmail.com>
2025-12-12 15:27:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -190,8 +190,8 @@ st.markdown("""
 # --- 4. GIAO DIỆN CHÍNH ---
 st.markdown("""
 <div class="main-header">
-    <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    TRỢ LÍ CỦA CÔ LIỄU
+    <p>Tác giả: Nguyễn Thị Liễu - Trường tiểu học Minh Dân - ĐT: 0359169611</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nguyễn Thị Liễu - Trường tiểu học Minh Dân - ĐT: 0359169611</div>", unsafe_allow_html=True)
